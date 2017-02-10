415f48ca7f45e46936f3ec042be1bf0ce247f821
Fcott <f.f.f.kt13@gmail.com>
2017-02-10 22:06:42 +0900

    add edit for room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_room, only: [:show, :edit, :update]
 
   def index
     @rooms = current_user.rooms
@@ -10,7 +11,6 @@ class RoomsController < ApplicationController
   end
 
   def show
-    @room = Room.find(params[:id])
     @user10 = @room.users.first(10)
     @user_names = @room.users.map(&:username).join(", ")
     @messages = @room.messages
@@ -18,7 +18,6 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
-
     if @room.save
       @room.user_rooms.where(user_id: current_user.id).first_or_create
       redirect_to @room, notice: 'Chatroom was successfully created.'
@@ -27,7 +26,22 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: 'Chatroom was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
   def room_params
     params.require(:room).permit(:name, :image)
   end
